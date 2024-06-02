b651fbd091de78c4acf4930f544a9edce13cbb4d
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-02 13:29:08 -0300

    Resolução do Desafio 03 - Encontrat o Maior e o Menor Elemento

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.1 - Lista/Desafio 3 - Encontrar o Maior e o Menor Elemento.py
@@ -0,0 +1,8 @@
+# Desafio: Encontre o maior e o menor elemento em uma lista [5, 2, 9, 1, 7, 6]. Dica: Use as funções max() e min().
+
+lista_numeros = [5, 2, 9, 1, 7, 6]
+maior_numero_lista = max(lista_numeros)
+menor_numero_lista = min(lista_numeros)
+
+print(f'O maior número desta lista é {maior_numero_lista}')
+print(f'O menor número desta lista é {menor_numero_lista}')
