3614e4f575b256ab760f35c73d9f0fe44a872625
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-02 13:04:56 -0300

    Resolução do Desafio 01 - Soma dos Elementos

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.1 - Lista/Desafio 1 - Soma de Elementos.py
@@ -0,0 +1,6 @@
+
+# Desafio: Dada uma lista de números [1, 2, 3, 4, 5], calcule a soma de todos os elementos. Dica: Use a função sum() para somar os elementos da lista.
+
+lista_numeros = [1, 2, 3, 4, 5]
+soma = sum(lista_numeros)
+print(soma)
