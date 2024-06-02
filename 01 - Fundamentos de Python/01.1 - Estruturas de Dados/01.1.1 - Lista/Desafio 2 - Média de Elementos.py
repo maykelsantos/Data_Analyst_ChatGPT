797de855a4b08a889bd4e1eb09d864a798ba4a50
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-02 13:23:03 -0300

    Resolução do Desafio 01 - Média de Elementos

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.1 - Lista/Desafio 2 - Média de Elementos.py
@@ -0,0 +1,7 @@
+# Desafio: Calcule a média dos elementos de uma lista de números [10, 20, 30, 40, 50]. Dica: A média é a soma dos elementos dividida pelo número de elementos.
+
+lista_numeros = [10, 20, 30, 40, 50]
+
+media_valores_lista = sum(lista_numeros) / 5
+print(f'A média dos valores desta lista é {media_valores_lista:.2f}')
+
