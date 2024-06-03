7cf0c552ae0b8478bc87ccbc95b4c7c25472c8b9
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-03 00:29:50 -0300

    Resolução do Desafio 016 - Obtenção de Todas as Chaves e Valores

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.2 - Dicionário/Desafio 016 - Obtenção de Todas as Chaves e Valores.py
@@ -0,0 +1,16 @@
+# Desafio: Extraia todas as chaves e todos os valores do dicionário alunos. Dica: Use alunos.keys() para as chaves e alunos.values() para os valores.
+
+alunos = {
+    'Maykel': 8.9,
+    'Vitoria': 10,
+    'Miguel': 8.7,
+    'Jaqueline': 9.5,
+    'Marcos': 7.5,
+    'Joana': 6.7,
+    'Maria': 5.9,
+    'Eloisa': 7.8,
+    'Elisa': 6.5,
+    'Rita': 9.8
+}
+
+print(alunos.keys(), alunos.values())
