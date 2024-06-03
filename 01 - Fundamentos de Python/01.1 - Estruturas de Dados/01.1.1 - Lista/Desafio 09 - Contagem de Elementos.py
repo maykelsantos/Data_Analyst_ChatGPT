4bdab274ee64211f488ecbe7ec52d24778066569
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-02 22:37:47 -0300

    Resolução do Desafio 09 - Contagem de Elementos

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.1 - Lista/Desafio 09 - Contagem de Elementos.py
@@ -0,0 +1,10 @@
+# Desafio: Conte quantas vezes um elemento específico aparece em uma lista ['a', 'b', 'c', 'a', 'b', 'a']. Dica: Use o método count() da lista.
+
+elementos = ['a', 'b', 'c', 'a', 'b', 'a']
+elemento1 = 'a'
+elemento2 = 'b'
+elemento3 = 'c'
+
+print(f'Contamos {elementos.count(elemento1)} vezes o elemento: {elemento1}')
+print(f'Contamos {elementos.count(elemento2)} vezes o elemento: {elemento2}')
+print(f'Contamos {elementos.count(elemento3)} vezes o elemento: {elemento3}')
