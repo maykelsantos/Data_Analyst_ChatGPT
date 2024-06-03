a7f593d511905169a89a0bdee4a70c91a659a55f
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-03 00:26:41 -0300

    Resolução do Deafio 015 - Iteração sobre um Dicionário

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.2 - Dicionário/Desafio 015 - Iteração sobre um Dicionário.py
@@ -0,0 +1,17 @@
+# Desafio: Itere sobre o dicionário alunos e imprima cada nome e sua respectiva nota. Dica: Utilize um loop for com alunos.items() para obter as chaves e valores.
+
+alunos = {
+    'Maykel': 8.9,
+    'Vitoria': 10,
+    'Miguel': 8.7,
+    'Jaqueline': 9.5,
+    'Marcos': 7.5,
+    'Joana': 6.7,
+    'Maria': 5.9,
+    'Eloisa': 7.8,
+    'Elisa': 6.5,
+    'Rita': 9.8
+}
+
+for dado in alunos.items():
+    print(dado)
