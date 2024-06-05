20e9485b418bef336f24d790414f55186f540a2f
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-04 22:52:05 -0300

    Resolução do Desafio 021 - Criação de Tuplas

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 021 - Criação de Tuplas.py
@@ -0,0 +1,6 @@
+# Tarefa: Crie uma tupla chamada meus_numeros que contenha os números de 1 a 5.
+# Dica: Use parênteses para definir uma tupla e separe os elementos por vírgulas.
+
+tuplas_01 = (1, 2, 3, 4, 5)
+
+print(tuplas_01)
