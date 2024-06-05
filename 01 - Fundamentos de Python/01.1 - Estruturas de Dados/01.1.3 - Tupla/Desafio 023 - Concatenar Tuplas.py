4e319d38e92d87e2552a72ef2465f81a42beb4fc
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-04 23:06:39 -0300

    Resolução do Desafio 023 - Concatenar Tuplas

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 023 - Concatenar Tuplas.py
@@ -0,0 +1,8 @@
+# Tarefa: Crie duas tuplas, tupla1 contendo os números de 1 a 3 e tupla2 contendo os números de 4 a 6. Em seguida, concatene-as para formar uma nova tupla chamada tupla3.
+# Dica: Use o operador + para concatenar tuplas.
+
+tupla1 = (1, 2, 3)
+tupla2 = (4, 5, 6)
+tupla3 = tupla1 + tupla2
+
+print(tupla3)
