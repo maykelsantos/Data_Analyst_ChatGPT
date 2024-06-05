2801f9aa882cab8e36da5ee00c92191833e30bbb
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2024-06-04 23:13:53 -0300

    Resolução do Desafio 025 - Verificação de Existência

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 025 - Verificação de Existência.py
@@ -0,0 +1,9 @@
+# Tarefa: Verifique se o número 4 está presente na tupla tupla3.
+# Dica: Use o operador in para verificar a existência de um elemento em uma tupla.
+
+tupla3 = (1, 2, 3, 4, 5, 6)
+
+if 6 in tupla3 == True:
+    print('O número 6 está presente na tupla3!')
+else:
+    print('O número 6 não está presente na tupla3!')
