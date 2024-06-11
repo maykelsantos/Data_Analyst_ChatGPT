2270e4976a90f6abc18eab64edc1a649389beba2
Maykel Santos <eumaykelandre@gmail.com>
2024-06-11 11:18:59 -0300

    Resolução do Desafio 027 - Desempacotamento de Tupla

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 027 - Desempacotamento de Tupla.py
@@ -0,0 +1,10 @@
+# Tarefa: Desempacote a tupla tupla4 em três variáveis a, b e c.
+# Dica: Atribua a tupla diretamente a três variáveis separadas por vírgulas.
+
+tupla4 = (1, 2, 3)
+
+a, b, c = tupla4
+
+print(f'A variável A contém o valor {a} da tupla.')
+print(f'A variável B contém o valor {b} da tupla.')
+print(f'A variável C contém o valor {c} da tupla.')
