aaa8fb35815603d832a10b8f6d0f3467142832ad
Maykel Santos <eumaykelandre@gmail.com>
2024-06-11 11:23:43 -0300

    Resolução do Desafio 028 - Tuplas Aninhadas

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 028 - Tuplas Aninhadas.py
@@ -0,0 +1,6 @@
+# Tarefa: Crie uma tupla chamada tupla_aninhada que contenha duas tuplas, cada uma com três elementos.
+# Dica: Uma tupla pode conter outros objetos, incluindo outras tuplas.
+
+tupla_aninhada = (('a', 'b', 'c'), (1, 2, 3))
+
+print(tupla_aninhada)
