302a9f68b0cd19ba9dce2b85d0c6f8fa1f8d3b4b
Maykel Santos <eumaykelandre@gmail.com>
2024-06-11 11:27:06 -0300

    Resolução do Desafio 029 - Iteração sobre Tupla

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 029 - Iteração sobre Tupla.py
@@ -0,0 +1,9 @@
+# Tarefa: Itere sobre a tupla tupla3 e imprima cada elemento.
+# Dica: Use um loop for para iterar sobre os elementos da tupla.
+import time
+
+tupla3 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+for valor in tupla3:
+    print(valor)
+    time.sleep(1)
