81c65dd322f8a42bf44170fb6f85313bbb249094
Maykel Santos <eumaykelandre@gmail.com>
2024-06-11 11:08:57 -0300

    Resolução do Desafio 026 - Conversão de Lista para Tupla

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 026 - Conversão de Lista para Tupla.py
@@ -0,0 +1,7 @@
+# Tarefa: Converta a lista [7, 8, 9] para uma tupla chamada tupla4.
+# Dica: Use a função tuple() para converter uma lista em uma tupla.
+
+lista = [7, 8, 9]
+tupla = tuple(lista)
+
+print(tupla)
