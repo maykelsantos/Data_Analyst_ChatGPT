8a2ef52358c3690d12302ca85ea06d17d3a1dbcf
Maykel Santos <eumaykelandre@gmail.com>
2024-06-11 11:29:27 -0300

    Resolução do Desafio 030 - Índice de um Elemento

---
+++ 01 - Fundamentos de Python/01.1 - Estruturas de Dados/01.1.3 - Tupla/Desafio 030 - Índice de um Elemento.py
@@ -0,0 +1,5 @@
+# Tarefa: Encontre o índice do número 5 na tupla tupla3.
+# Dica: Use o método .index() da tupla para encontrar o índice de um elemento.
+
+tupla3 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tupla3.index(5))
